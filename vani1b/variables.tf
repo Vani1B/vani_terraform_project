77fecae0174c959edb7e5d10123b1ae261543b95
Vani1B <135303019+Vani1B@users.noreply.github.com>
2025-04-11 22:46:10 +0530

    Add files via upload

---
+++ vani1b/variables.tf
@@ -0,0 +1,10 @@
+variable "my_ami" {
+  description = "This to define my ami"
+  default     = "ami-0c02fb55956c7d316"
+}
+
+variable "my_instance_type" {
+  description = "This to define my instance type"
+  default = "t2.micro"
+
+}
